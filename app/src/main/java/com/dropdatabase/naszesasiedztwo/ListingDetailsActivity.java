fcd2dcbb30523a38604ae3ab83827b413451264f
Jacek Jarosz <jacekjarosz55@gmail.com>
2022-10-21 21:04:34 +0200

    another fix

--- app/src/main/java/com/dropdatabase/naszesasiedztwo/ListingDetailsActivity.java
+++ app/src/main/java/com/dropdatabase/naszesasiedztwo/ListingDetailsActivity.java
@@ -10,5 +10,8 @@ public class ListingDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing_details);
+
+
+
     }
 }
